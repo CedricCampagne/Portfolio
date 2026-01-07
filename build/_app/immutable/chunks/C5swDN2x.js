9bc9eb4ad8f26e75fda20b2aeb7b70350890bf22
CedricCampagne <ced.campagne@gmail.com>
2026-01-07 13:01:45 +0100

    add build pour GitHub Pages

---
+++ build/_app/immutable/chunks/C5swDN2x.js
@@ -0,0 +1,1 @@
+import{i as a}from"./DqUL6p-w.js";a();
---
+++ build/_app/immutable/chunks/DSEbtqyw.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_l1qzmd?.base??"/Portfolio",t=globalThis.__sveltekit_l1qzmd?.assets??s??"";export{t as a,s as b};
