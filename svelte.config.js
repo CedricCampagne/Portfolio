579c75be7efeec9b756661a8c8f183ad33a549a2
CedricCampagne <ced.campagne@gmail.com>
2026-04-02 14:09:30 +0200

    svelte.config update runetime

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 const config = {
   kit: {
     adapter: adapter({
-      // options par défaut suffisent
-      edge: false
+      runtime: 'nodejs'
     })
   }
 };
